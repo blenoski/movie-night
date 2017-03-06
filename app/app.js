2e5e81908b494c470c6a9982d23c8301bac074b1
Brian Lenoski <brian.lenoski@gmail.com>
2017-03-06 12:02:47 -0600

    Integrate Babel and Webpack including support for ES6 and JSX

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  React.createElement('h1', {}, 'Movie Night!!!'),
+  <h1>Movie Night!!!</h1>,
   document.getElementById('root')
 );
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './app/app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'app', 'dist'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: [
+          path.resolve(__dirname, "app")
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: ["es2015", "react"]
+        }
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, "app")
+    ],
+    extensions: [".js", ".jsx"]
+  }
+};
