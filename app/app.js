0a7f6954b3789a18cd2dc582ed49d35e0cae85b9
Brian Lenoski <brian.lenoski@gmail.com>
2017-03-05 19:15:47 -0600

    Minimal integratation of React and ReactDOM

---
+++ app/app.js
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  React.createElement('h1', {}, 'Movie Night!!!'),
+  document.getElementById('root')
+);
