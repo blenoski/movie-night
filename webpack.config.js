098731ea0750d8bc97007a362deaaa49fa465e8d
Brian Lenoski <brian.lenoski@gmail.com>
2017-03-07 14:02:27 -0600

    Added webpack-dev-server including support for live reloading, separate production and development builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+console.log('NODE_ENV=' + process.env.NODE_ENV)
+
+const bundleName = (process.env.NODE_ENV === 'production'
+  ? 'bundle.js'
+  : 'http://localhost:3000/dist/bundle.js')
+console.log('Using bundled javascript source: ', bundleName)
 
 module.exports = {
   entry: {
     main: './app/app.js'
   },
   output: {
-    path: path.resolve(__dirname, 'app', 'dist'),
-    filename: 'app.bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
@@ -17,7 +25,7 @@ module.exports = {
         ],
         loader: 'babel-loader',
         options: {
-          presets: ['es2015', 'react']
+          presets: [['env', {'modules': false}], 'react']
         }
       }
     ]
@@ -28,5 +36,13 @@ module.exports = {
       path.resolve(__dirname, 'app')
     ],
     extensions: ['.js', '.jsx']
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'app', 'index-template.html'),
+      bundle: bundleName,
+      inject: false
+    })
+  ]
 }
