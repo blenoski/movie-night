22a6f397a1c35edfb991127caa26c07e218eb05f
Brian Lenoski <brian.lenoski@gmail.com>
2017-03-14 08:31:19 -0600

    Removed original/dead movie-night project

--- app/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-
-/* globals test  */
-test('App renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
