10e31cdae21c6126170d2cba8f8dd75e67ad8e04
Brian Lenoski <brian.lenoski@gmail.com>
2017-03-16 18:17:19 -0600

    Make bootstrap an npm dependency

--- app/index.js
+++ app/index.js
@@ -4,8 +4,8 @@ import App from './App'
 
 // Import the Bootstrap styles.
 // This will make the bootstrap styling default.
-// Importing uniminified version so we can let Webpack handle minimization.
-import '../../bootstrap/bootstrap-4.0.0-alpha.6-dist/css/bootstrap.css'
+// Importing unminified version so we can let Webpack handle minimization.
+import 'bootstrap/dist/css/bootstrap.css'
 
 // Render the App.
 ReactDOM.render(
