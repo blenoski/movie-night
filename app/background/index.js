380bf933b1430e0b4b373ced33d5467859038989
Brian Lenoski <brian.lenoski@gmail.com>
2017-04-23 16:32:59 -0500

    [movie-night] Allow loaded DB to garbage collected when handlers complete. Do not send CRAWL_COMPLETE until all movie files have been processed

--- app/background/index.js
+++ app/background/index.js
@@ -20,75 +20,23 @@ const { checkIfPosterHasBeenDownloadedFor, downloadPosterFor } = require('./post
 const APPDATA_PATH = '/Users/blenoski/Developer/ConfidentCruiser/confident-cruiser/movie-night/appdata'
 const DB_PATH = `${APPDATA_PATH}/database`
 const dbFile = 'movieDB.json'
+const dbConfig = { dbPath: DB_PATH, uniqueField: 'imdbID', dbFile }
 
 // Record environment.
 logEnv(logger)
 
-// Instantiate the database.
-let db = new SingleCollectionDatabase({ dbPath: DB_PATH, uniqueField: 'imdbID', dbFile })
-logger.info('Database config:', db.config())
-
-// Called upon ENTERING a new crawl directory
-const searchDirCb = (directory) => {
-  ipcRenderer.send(SEARCHING_DIRECTORY, directory)
-  logger.debug('Sent SEARCHING_DIRECTORY event', { directory })
-}
-
-// Handler for CRAWL_DIRECTORY events.
-ipcRenderer.on(CRAWL_DIRECTORY, (event, rootDirectory) => {
-  logger.info('Received CRAWL_DIRECTORY event', { rootDirectory })
-
-  crawlForMovies({rootDirectory, searchDirCb, movieFileCb})
-    .then(() => {
-      // TODO: wait for all movies to finish processing
-      ipcRenderer.send(CRAWL_COMPLETE, rootDirectory)
-      logger.info('Sent CRAWL_COMPLETE event', { rootDirectory })
-    })
-})
-
-// Called whenever a movie file is encountered during crawl.
-function movieFileCb (movieFile) {
-  logger.info('Found', { movie: movieFile })
-
-  // Early exit if this movie file is already in the database.
-  // const document = db.findByLocation(movieFile)
-  const document = db.findOne(movieWithAnyLocationMatching(movieFile))
-  if (document) {
-    logger.info(`Database has existing record for ${movieFile}`, {
-      title: document.title,
-      imdbID: document.imdbID
-    })
-    return
-  }
-
-  fetchMovieMetadata(movieFile)
-    .then((movie) => {
-      return checkIfPosterHasBeenDownloadedFor(movie)
-    })
-    .then(({posterDownloaded, movie}) => {
-      let document = db.findByID(movie.imdbID)
-      if (posterDownloaded && document && movie.imgUrl === document.imgUrl) {
-        return {movie, document} // poster image is already good to go
-      }
-      return downloadPosterFor(movie).then(() => { return {movie, document} })
-    })
-    .then(({movie, document}) => {
-      let {documentChanged, finalDocument} = conflate(document, movie)
-      if (documentChanged) {
-        let movieDB = db.addOrUpdate(finalDocument)
-        ipcRenderer.send(MOVIE_DATABASE, movieDB) // SUCCESS!
-        logger.info('Sent MOVIE_DATABASE event with new movie', { title: movie.title, count: movieDB.length })
-      }
-    })
-    .catch((err) => {
-      logger.warn(`${movieFile} not added to database:`, { type: err.name, message: err.message })
-      // TODO: send an data not found event to appWindow?
-    })
-}
-
+// ============================================================
 // Handle the LOAD_MOVIE_DATABASE event called once at startup.
+// ============================================================
 ipcRenderer.once(LOAD_MOVIE_DATABASE, (event) => {
   logger.info('Received LOAD_MOVIE_DATABASE event')
+
+  // Instantiate the database.
+  // The loaded database should be garbage collected upon completion of handler.
+  let db = new SingleCollectionDatabase(dbConfig)
+  logger.info('Database config:', db.config())
+
+  // Send the movie database
   let movieDB = db.getCollection()
   ipcRenderer.send(MOVIE_DATABASE, movieDB) // SUCCESS!
   logger.info('Sent MOVIE_DATABASE event', { count: movieDB.length })
@@ -103,6 +51,98 @@ ipcRenderer.once(LOAD_MOVIE_DATABASE, (event) => {
       })
   })
 })
+
+// ===================================
+// Handler for CRAWL_DIRECTORY events.
+// ===================================
+ipcRenderer.on(CRAWL_DIRECTORY, (event, rootDirectory) => {
+  logger.info('Received CRAWL_DIRECTORY event', { rootDirectory })
+
+  // Instantiate the database.
+  // The loaded database should get garbage collected upon completion of handler.
+  let db = new SingleCollectionDatabase(dbConfig)
+  logger.info('Database config:', db.config())
+
+  // As we discover movie files during the crawl, we will push Promises
+  // onto this array which represent the work of downloading the metadata
+  // for a single movie. When the crawl is complete, we can then sequentially
+  // wait on each Promise in the array using reduce. When the last Promise has
+  // completed we are finished. Note that this does not prevent the Promises
+  // from running in parallel.
+  let moviesInProgress = []
+
+  const movieFileCb = (movieFile) => {
+    logger.info('Found', { movie: movieFile })
+    moviesInProgress.push(addMovie(movieFile, db))
+  }
+
+  crawlForMovies({rootDirectory, searchDirCb, movieFileCb})
+    .then(() => {
+      // Wait for all movies to finish processing before
+      // returning from crawl
+      return moviesInProgress.reduce((prev, curr) => {
+        return prev.then(() => {
+          return curr.then(({err, movieFile}) => {
+            if (err) {
+              logger.warn(`${movieFile} not added to database:`, { type: err.name, message: err.message })
+              // TODO: send a data not found event to appWindow?
+            } else {
+              logger.info(`completed ${movieFile}`)
+            }
+          })
+        })
+      }, Promise.resolve())
+    })
+    .then(() => {
+      ipcRenderer.send(CRAWL_COMPLETE, rootDirectory)
+      logger.info('Sent CRAWL_COMPLETE event', { rootDirectory })
+    })
+})
+
+// Called upon ENTERING a new crawl directory
+const searchDirCb = (directory) => {
+  ipcRenderer.send(SEARCHING_DIRECTORY, directory)
+  logger.debug('Sent SEARCHING_DIRECTORY event', { directory })
+}
+
+// Called whenever a movie file is encountered during crawl.
+function addMovie (movieFile, db) {
+  return new Promise((resolve, reject) => {
+    // Early exit if this movie file is already in the database.
+    const document = db.findOne(movieWithAnyLocationMatching(movieFile))
+    if (document) {
+      logger.info(`Database has existing record for ${movieFile}`, {
+        title: document.title,
+        imdbID: document.imdbID
+      })
+      return resolve({err: null, movieFile})
+    }
+
+    return fetchMovieMetadata(movieFile)
+      .then((movie) => {
+        return checkIfPosterHasBeenDownloadedFor(movie)
+      })
+      .then(({posterDownloaded, movie}) => {
+        let document = db.findByID(movie.imdbID)
+        if (posterDownloaded && document && movie.imgUrl === document.imgUrl) {
+          return {movie, document} // poster image is already good to go
+        }
+        return downloadPosterFor(movie).then(() => { return {movie, document} })
+      })
+      .then(({movie, document}) => {
+        let {documentChanged, finalDocument} = conflate(document, movie)
+        if (documentChanged) {
+          let movieDB = db.addOrUpdate(finalDocument)
+          ipcRenderer.send(MOVIE_DATABASE, movieDB) // SUCCESS!
+          logger.info('Sent MOVIE_DATABASE event with new movie', { title: movie.title, count: movieDB.length })
+        }
+        return resolve({err: null, movieFile})
+      })
+      .catch((error) => {
+        return resolve({err: error, movieFile})
+      })
+  })
+}
 
 // Conflate a new movie with existing database document.
 // TODO: delegate this to a proper meta class, e.g., conflate method
