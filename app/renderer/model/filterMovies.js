e7e26a19c12eef9a1591f333f5e3541f6cfdd1a7
Brian Lenoski <brian.lenoski@gmail.com>
2017-05-15 14:57:13 -0500

    Move filtering logic to model

--- app/renderer/model/filterMovies.js
+++ app/renderer/model/filterMovies.js
@@ -1,41 +1,78 @@
-// Logic for taking the movie database and filtering it by a search query.
-export default (searchQuery, movies) => {
-  let filteredMovies = movies
-  if (searchQuery) {
-    // WARNING:
-    // Tight inner loop executing in real time as user is typing search query.
-    // Performance really matters inside this block.
-    searchQuery = searchQuery.toLowerCase().trim()
+// WARNING: Performance really matters inside this module.
+// Execution happens in real time as user is typing search query and/or
+// as movies are added to the database. Execution is currently blocking to
+// allow for real-time search results. Could make async at the expense of needing
+// a spinner for search results and logic for what happens when movies get added
+// to database at the same time search query is being updated. Make sure to keep
+// the algorithm O(n) and only iterate through the entire database once.
 
-    filteredMovies = filteredMovies.filter((movie) => {
-      if (movie.title.toLowerCase().indexOf(searchQuery) >= 0) {
-        return true
+export default function (searchQuery, movies) {
+  searchQuery = searchQuery.toLowerCase().trim()
+
+  // TODO: Movie this work to background thread minus the search filter.
+  // I.e. movies should come into render thread already indexed by genre and sorted.
+  // Loop over movies, filter on search query and partition into genres.
+  let genreMap = {}
+  let noPosters = []
+  let moviesLength = movies.length
+  for (let i = 0; i < moviesLength; i += 1) {
+    const movie = movies[i]
+    if (!searchQuery || searchFilterIncludes(movie, searchQuery)) {
+      if (movie.imgFile) {
+        const genre = movie.genres[0]
+        let items = genreMap[genre]
+        items ? items.push(movie) : genreMap[genre] = [movie]
+      } else {
+        noPosters.push(movie)
       }
-
-      // Look for matching genre.
-      // Here we only consider the first/primary genre and
-      // we are looking for a startsWith match.
-      if (movie.genres[0].startsWith(searchQuery)) {
-        return true
-      }
-
-      // Look for matching actor. Use traditional for loop for performance.
-      const actors = movie.actors
-      let actorsLength = actors.length
-      for (let j = 0; j < actorsLength; j += 1) {
-        if (actors[j].indexOf(searchQuery) >= 0) {
-          return true
-        }
-      }
-
-      // Look for matching director.
-      if (movie.director.toLowerCase().indexOf(searchQuery) >= 0) {
-        return true
-      }
-
-      return false
-    })
+    }
   }
 
-  return filteredMovies
+  // Push movies with no poster to end of genre array.
+  // Assumption is this array will be small.
+  for (let j = 0; j < noPosters.length; j += 1) {
+    const movie = noPosters[j]
+    const genre = movie.genres[0]
+    let items = genreMap[genre]
+    items ? items.push(movie) : genreMap[genre] = [movie]
+  }
+
+  // Order by genre from most movies to least movies.
+  return Object.keys(genreMap).sort((lhs, rhs) => {
+    const countRhs = genreMap[rhs].length
+    const countLhs = genreMap[lhs].length
+    return countRhs - countLhs
+  }).map(genre => {
+    return { genre, movies: genreMap[genre] }
+  })
+}
+
+function searchFilterIncludes (movie, query) {
+  // Look for matching title
+  if (movie.title.toLowerCase().indexOf(query) >= 0) {
+    return true
+  }
+
+  // Look for matching genre.
+  // Here we only consider the first/primary genre and
+  // we are looking for a startsWith match.
+  if (movie.genres[0].startsWith(query)) {
+    return true
+  }
+
+  // Look for matching actor. Use traditional for loop for performance.
+  const actors = movie.actors
+  let actorsLength = actors.length
+  for (let j = 0; j < actorsLength; j += 1) {
+    if (actors[j].indexOf(query) >= 0) {
+      return true
+    }
+  }
+
+  // Look for matching director.
+  if (movie.director.toLowerCase().indexOf(query) >= 0) {
+    return true
+  }
+
+  return false
 }
--- app/renderer/views/DisplayMovies/index.js
+++ app/renderer/views/DisplayMovies/index.js
@@ -16,66 +16,34 @@ export default class DisplayMovies extends Component {
     }
   }
 
-  rankedGenres () {
-    // Create a histogram of title counts for each genre.
-    const genreMap = new Map()
-    this.props.movies.forEach((movie) => {
-      const genre = movie.genres[0] // get first genre
-      const count = genreMap.get(genre) || 0
-      genreMap.set(genre, count + 1)
-    })
-
-    // Sort genres in order from most titles to least titles.
-    const genres = [ ...genreMap ].sort((lhs, rhs) => {
-      const countRhs = rhs[1]
-      const countLhs = lhs[1]
-      return countRhs - countLhs
-    }).map((genre) => {
-      return genre[0]
-    })
-
-    return genres
-  }
-
   render () {
     const { movies, closeMovieDetailsWhenOnlyOneMovie } = this.props
 
-    if (movies.length <= 0) {
+    if (movies.length === 0) {
       return null
     }
 
     if (movies.length === 1) {
-      return (
-        <MovieDetail
-          movie={movies[0]}
-          handleCloseMovieDetails={closeMovieDetailsWhenOnlyOneMovie}
-        />
-      )
+      const moviesFirstGenre = movies[0].movies
+      if (moviesFirstGenre.length === 1) {
+        return (
+          <MovieDetail
+            movie={moviesFirstGenre[0]}
+            handleCloseMovieDetails={closeMovieDetailsWhenOnlyOneMovie}
+          />
+        )
+      }
     }
 
     return (
       <MoviesGroupedByGenre>
-        { this.rankedGenres().map(genre => this.renderMovieByGenre(genre)) }
+        { movies.map(item => this.renderMovies(item)) }
       </MoviesGroupedByGenre>
     )
   }
 
-  renderMovieByGenre (genre) {
-    // Grab the movies in this genre.
-    // Sort any movies with no poster to the end.
-    const movies = this.props.movies.filter((movie) => {
-      return movie.genres[0] === genre
-    })
-    .sort((movieLhs, movieRhs) => {
-      if (movieLhs.imgFile && !movieRhs.imgFile) {
-        return -1
-      } else if (movieRhs.imgFile && !movieLhs.imgFile) {
-        return 1
-      } else {
-        return 0
-      }
-    })
-
+  renderMovies (item) {
+    const { genre, movies } = item
     return (
       <div key={genre}>
         <MovieGallery key={genre} genre={genre} movies={movies} />
