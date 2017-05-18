ccae2805af21de5147bfae8143adc259951b4950
Brian Lenoski <brian.lenoski@gmail.com>
2017-05-18 14:55:44 -0500

    Add tests for shared/events.js and shared/utils.js

--- __mocks__/electron.js
+++ __mocks__/electron.js
@@ -1,7 +1,7 @@
+// Electron modules must be run in an an electron process. Jest runs
+// in a node process.  Thus, we will mock out the Electron modules here.
 // https://facebook.github.io/jest/docs/manual-mocks.html
-// 'use strict'
-// const electron = jest.genMockFromModule('electron') /* globals jest */
-// module.exports = electron
+'use strict'
 
 const ipcRenderer = {
   on: jest.genMockFunction()
@@ -9,10 +9,5 @@ const ipcRenderer = {
 
 /* globals jest */
 module.exports = {
-  require: jest.genMockFunction(),
-  match: jest.genMockFunction(),
-  app: jest.genMockFunction(),
-  remote: jest.genMockFunction(),
-  dialog: jest.genMockFunction(),
   ipcRenderer
 }
---
+++ app/shared/__tests__/utils.js
@@ -0,0 +1,167 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+
+const {
+  ExtendableError,
+  fileExists,
+  isDevEnv,
+  logEnv,
+  lstat,
+  mkdir,
+  readdir,
+  writeFile
+} = require('../utils')
+
+/* globals describe, test, expect, jest, beforeAll, afterAll */
+
+describe('utils', () => {
+  describe('ExtendableError', () => {
+    test('base class name and message properties', () => {
+      const errorMsg = 'base error message'
+      const error = new ExtendableError(errorMsg)
+      expect(error).toBeInstanceOf(ExtendableError)
+      expect(error.name).toBe('ExtendableError')
+      expect(error.message).toBe(errorMsg)
+    })
+
+    test('derived class overrides name', () => {
+      class DerivedError extends ExtendableError {}
+      const errorMsg = 'derived error message'
+      const error = new DerivedError(errorMsg)
+      expect(error).toBeInstanceOf(DerivedError)
+      expect(error.name).toBe('DerivedError')
+      expect(error.message).toBe(errorMsg)
+    })
+  })
+
+  describe('fileExists', () => {
+    test('returns true if file exists', () => {
+      return expect(fileExists(__filename)).resolves.toBe(true)
+    })
+
+    test('returns false if file does not exist', () => {
+      const badFile = `${__filename}gobblygook`
+      return expect(fileExists(badFile)).resolves.toBe(false)
+    })
+  })
+
+  describe('environment', () => {
+    test('this is test environment', () => {
+      expect(process.env.NODE_ENV).toBe('test')
+      expect(isDevEnv()).toBe(false)
+    })
+
+    test('dev env recognized', () => {
+      const orig = process.env.NODE_ENV
+      process.env.NODE_ENV = 'development'
+      expect(isDevEnv()).toBe(true)
+      process.env.NODE_ENV = orig
+      expect(process.env.NODE_ENV).toBe(orig)
+    })
+
+    test('logEnv calls logger.info with env', () => {
+      const loggerMock = { info: jest.fn() }
+      logEnv(loggerMock)
+      expect(loggerMock.info.mock.calls.length).toBe(1)
+      expect(loggerMock.info.mock.calls[0]).toEqual(
+        [
+          '', // first arg
+          { // second arg
+            'LOG_LEVEL': process.env.LOG_LEVEL,
+            'NODE_ENV': process.env.NODE_ENV
+          }
+        ]
+      )
+    })
+  })
+
+  describe('lstat', () => {
+    test('resolves with stats object if file exists', () => {
+      return expect(lstat(__filename)).resolves.toBeInstanceOf(fs.Stats)
+    })
+
+    test('rejects when file does not exist', () => {
+      return expect(lstat('/badfile')).rejects.toBeDefined()
+    })
+  })
+
+  describe('mkdir', () => {
+    const testmkdir = path.join(__dirname, 'testmkdir')
+    const cleanup = () => {
+      try {
+        fs.rmdirSync(testmkdir)
+      } catch (err) {
+        ;
+      }
+    }
+    beforeAll(() => cleanup())
+
+    test('creates directory if not existing', () => {
+      return expect(mkdir(testmkdir)).resolves.toBeUndefined()
+    })
+
+    test('succeeds when directory already exists', () => {
+      return expect(mkdir(testmkdir)).resolves.toBeUndefined()
+    })
+
+    test('rejects when directory cannot be created', () => {
+      return expect(mkdir('/badpath')).rejects.toBeDefined()
+    })
+
+    afterAll(() => cleanup())
+  })
+
+  describe('readdir', () => {
+    test('resolves with directory items if directory exists', () => {
+      return expect(readdir(__dirname)).resolves.toContain('__snapshots__', 'utils.js')
+    })
+
+    test('rejects when directory does not exist', () => {
+      return expect(readdir('/badpath')).rejects.toBeDefined()
+    })
+  })
+
+  describe('writeFile', () => {
+    const testfile = path.join(__dirname, 'testfile')
+    const cleanup = () => {
+      try {
+        fs.unlinkSync(testfile)
+      } catch (err) {
+        ;
+      }
+    }
+    beforeAll(() => cleanup())
+
+    const data = 'testdata'
+    test('creates and writes file if not existing', () => {
+      return expect(writeFile(testfile, data)).resolves.toBeUndefined()
+    })
+    test('correct data is written', (done) => {
+      fs.readFile(testfile, 'utf8', (err, fileContents) => {
+        if (err) throw err
+        expect(fileContents).toBe(data)
+        done()
+      })
+    })
+
+    const overwriteData = 'overwrites'
+    test('overwrites file if existing', () => {
+      return expect(writeFile(testfile, overwriteData)).resolves.toBeUndefined()
+    })
+    test('correct data is overwritten', (done) => {
+      fs.readFile(testfile, 'utf8', (err, fileContents) => {
+        if (err) throw err
+        expect(fileContents).toBe(overwriteData)
+        done()
+      })
+    })
+
+    test('rejects when file cannot be created', () => {
+      return expect(writeFile('/badpath')).rejects.toBeDefined()
+    })
+
+    afterAll(() => cleanup())
+  })
+})
